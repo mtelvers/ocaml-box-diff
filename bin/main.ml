dba3f46c27d81ffb04bdbbf90cee4d52d4757e02
Mark Elvers <mark.elvers@tunbury.org>
2025-03-28 15:14:43 +0000

    Now check files as well

--- bin/main.ml
+++ bin/main.ml
@@ -30,14 +30,19 @@ let field f j = Yojson.Safe.Util.member f j |> Yojson.Safe.Util.to_string
 
 module Entries = Map.Make (String)
 
-let lst env token id =
+let read_from_file filename =
+  In_channel.with_open_text filename @@ fun ic -> In_channel.input_all ic
+
+let rec box_get_folder env token_file id =
+  let token = read_from_file token_file |> String.trim in
   let client = Client.make ~https:(Some (https ~authenticator)) env#net in
   Eio.Switch.run @@ fun sw ->
   let headers = Http.Header.init () in
   let headers = Http.Header.add headers "authorization" ("Bearer " ^ token) in
   let resp, body =
     Client.get ~sw ~headers client
-      (Uri.of_string ("https://api.box.com/2.0/folders/" ^ (string_of_int id) ^ "/items"))
+      (Uri.of_string
+         ("https://api.box.com/2.0/folders/" ^ string_of_int id ^ "/items"))
   in
   if Http.Status.compare resp.status `OK = 0 then
     let json =
@@ -48,8 +53,7 @@ let lst env token id =
     |> Yojson.Safe.Util.to_list
     |> List.filter_map (fun entry ->
            match field "type" entry with
-           | "file" -> None
-           | "folder" ->
+           | "file" | "folder" ->
                let id = field "id" entry |> int_of_string in
                let name = field "name" entry in
                Some (name, id)
@@ -57,57 +61,68 @@ let lst env token id =
                Printf.printf "Unknown %s\n" s;
                assert false)
   else
-    assert false
-      (* Fmt.epr "Unexpected HTTP status: %a" Http.Status.pp resp.status *)
+    let () = Unix.sleep 60 in
+    let () = Fmt.epr "Unexpected HTTP status: %a" Http.Status.pp resp.status in
+    box_get_folder env token_file id
 
-let cache = Hashtbl.create 10000
+let cache = Hashtbl.create 100000
 
-let rec box_check env token id = function
+let rec box_check env token_file id = function
   | [] ->
-        let () = Printf.printf "OK\n%!" in true
+      let () = Printf.printf "OK\n%!" in
+      true
   | hd :: tl -> (
       let entries =
         match Hashtbl.find_opt cache id with
         | Some entries -> entries
         | None ->
-            let map = lst env token id |> Entries.of_list in
+            let map = box_get_folder env token_file id |> Entries.of_list in
             let () = Hashtbl.add cache id map in
             map
       in
       match Entries.find_opt hd entries with
-      | Some id -> box_check env token id tl
+      | Some id -> box_check env token_file id tl
       | None ->
-        let () = Printf.printf "Not found\n%!" in
-        false)
+          let () = Printf.printf "Not found\n%!" in
+          false)
 
-let scan env token src dst p =
+let scan env token_file src dst p =
   let rec loop = function
     | hd :: tl when Sys.is_directory (src / hd) ->
         (* does hd exist in box? *)
         let () = Printf.printf "Check box for path %s - %!" (dst / hd) in
-        let v = box_check env token 0 (String.split_on_char '/' (dst / hd)) in
+        let v =
+          box_check env token_file 0 (String.split_on_char '/' (dst / hd))
+        in
         if v then
-        Sys.readdir (src / hd) |> Array.to_list
-        |> List.map (Filename.concat hd)
-        |> List.append tl |> loop
+          let d =
+            Sys.readdir (src / hd)
+            |> Array.to_list
+            |> List.map (Filename.concat hd)
+          in
+          loop (d @ tl)
         else loop tl
-    (*| hd :: tl -> hd :: loop tl *)
-    | _ :: tl -> loop tl
+    | hd :: tl ->
+        let () = Printf.printf "Check box for file %s - %!" (dst / hd) in
+        let _ =
+          box_check env token_file 0 (String.split_on_char '/' (dst / hd))
+        in
+        loop tl
     | [] -> []
   in
   loop [ p ]
 
-let call env token _ =
+let call env token_file _ =
   Mirage_crypto_rng_unix.use_default ();
-  let _ = scan env token "/data2/Assets" "Data/Assets" "Dunlin"
-  in ()
+  let _ = scan env token_file "/data2/Assets" "Data/Assets" "Dunlin" in
+  ()
 
 open Cmdliner
 
-let token =
+let token_file =
   Arg.required
   @@ Arg.opt Arg.(some string) None
-  @@ Arg.info [ "token" ] ~docv:"TOKEN" ~doc:"API token"
+  @@ Arg.info [ "token-file" ] ~docv:"TOKEN" ~doc:"API token"
 
 let id =
   Arg.required
@@ -117,6 +132,6 @@ let id =
 let prog env =
   let doc = "Mirror Box API" in
   let info = Cmd.info "query" ~doc in
-  Cmd.v info Term.(const (call env) $ token $ id)
+  Cmd.v info Term.(const (call env) $ token_file $ id)
 
 let _ = Eio_main.run @@ fun env -> Cmd.eval (prog env)
